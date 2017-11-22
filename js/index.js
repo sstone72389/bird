8ed3aa46ecb47a9481dd662392f14c37ba4a8ef3
“sstone72389” <sstone72389@gmail.com>
2017-11-22 12:50:38 -0500

    inital commit

---
+++ js/index.js
@@ -0,0 +1,82 @@
+$(function(){
+  $('.welcome').css('display', 'none');
+  $('.welcome').fadeIn(1000);
+});
+
+$(".next").on("click", function(){
+  $(".welcome").hide()
+  $('.slide').css('display', 'none');
+  $('.slide').fadeIn(1000);
+});
+
+$(".next1").on("click", function(){
+  $(".slide").hide()
+  $('.slide2').css('display', 'none');
+  $('.slide2').fadeIn(1000);
+});
+
+$(".prev1").on("click", function(){
+  $(".slide").hide()
+  $('.welcome').css('display', 'none');
+  $('.welcome').fadeIn(1000);
+});
+
+$(".next2").on("click", function(){
+  $(".slide2").hide()
+  $('.slide3').css('display', 'none');
+  $('.slide3').fadeIn(1000);
+});
+
+$(".prev2").on("click", function(){
+  $(".slide2").hide()
+  $('.slide').css('display', 'none');
+  $('.slide').fadeIn(1000);
+});
+
+$(".next3").on("click", function(){
+  $(".slide3").hide()
+  $('.slide4').css('display', 'none');
+  $('.slide4').fadeIn(1000);
+});
+
+$(".prev3").on("click", function(){
+  $(".slide3").hide()
+  $('.slide2').css('display', 'none');
+  $('.slide2').fadeIn(1000);
+});
+
+$(".next4").on("click", function(){
+  $(".slide4").hide()
+  $('.slide5').css('display', 'none');
+  $('.slide5').fadeIn(1000);
+});
+
+$(".prev4").on("click", function(){
+  $(".slide4").hide()
+  $('.slide3').css('display', 'none');
+  $('.slide3').fadeIn(1000);
+});
+
+$(".next5").on("click", function(){
+  $(".slide5").hide()
+  $('.slide6').css('display', 'none');
+  $('.slide6').fadeIn(1000);
+});
+
+$(".prev5").on("click", function(){
+  $(".slide5").hide()
+  $('.slide4').css('display', 'none');
+  $('.slide4').fadeIn(1000);
+});
+
+$(".next6").on("click", function(){
+  $(".slide6").hide()
+  $('.slide7').css('display', 'none');
+  $('.slide7').fadeIn(1000);
+});
+
+$(".prev6").on("click", function(){
+  $(".slide6").hide()
+  $('.slide5').css('display', 'none');
+  $('.slide5').fadeIn(1000);
+});
