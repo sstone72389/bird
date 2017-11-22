ff32217c65099875daa1f412aa8fd9a632a5c58e
“sstone72389” <sstone72389@gmail.com>
2017-11-22 15:03:33 -0500

    completes instructions

--- js/index.js
+++ js/index.js
@@ -5,18 +5,18 @@ $(function(){
 
 $(".next").on("click", function(){
   $(".welcome").hide()
-  $('.slide').css('display', 'none');
-  $('.slide').fadeIn(1000);
+  $('.intro-slide').css('display', 'none');
+  $('.intro-slide').fadeIn(1000);
 });
 
 $(".next1").on("click", function(){
-  $(".slide").hide()
+  $(".intro-slide").hide()
   $('.slide2').css('display', 'none');
   $('.slide2').fadeIn(1000);
 });
 
 $(".prev1").on("click", function(){
-  $(".slide").hide()
+  $(".intro-slide").hide()
   $('.welcome').css('display', 'none');
   $('.welcome').fadeIn(1000);
 });
@@ -29,8 +29,9 @@ $(".next2").on("click", function(){
 
 $(".prev2").on("click", function(){
   $(".slide2").hide()
-  $('.slide').css('display', 'none');
-  $('.slide').fadeIn(1000);
+  $(".slide2").hide()
+  $('.intro-slide').css('display', 'none');
+  $('.intro-slide').fadeIn(1000);
 });
 
 $(".next3").on("click", function(){
@@ -81,8 +82,86 @@ $(".prev6").on("click", function(){
   $('.slide5').fadeIn(1000);
 });
 
+$(".next7").on("click", function(){
+  $(".slide7").hide()
+  $('.slide8').css('display', 'none');
+  $('.slide8').fadeIn(1000);
+});
+
 $(".prev7").on("click", function(){
   $(".slide7").hide()
   $('.slide6').css('display', 'none');
   $('.slide6').fadeIn(1000);
+});
+
+$(".next8").on("click", function(){
+  $(".slide8").hide()
+  $('.slide9').css('display', 'none');
+  $('.slide9').fadeIn(1000);
+});
+
+$(".prev8").on("click", function(){
+  $(".slide8").hide()
+  $('.slide7').css('display', 'none');
+  $('.slide7').fadeIn(1000);
+});
+
+$(".next9").on("click", function(){
+  $(".slide9").hide()
+  $('.slide10').css('display', 'none');
+  $('.slide10').fadeIn(1000);
+});
+
+$(".prev9").on("click", function(){
+  $(".slide9").hide()
+  $('.slide8').css('display', 'none');
+  $('.slide8').fadeIn(1000);
+});
+
+$(".next10").on("click", function(){
+  $(".slide10").hide()
+  $('.slide11').css('display', 'none');
+  $('.slide11').fadeIn(1000);
+});
+
+$(".prev10").on("click", function(){
+  $(".slide10").hide()
+  $('.slide9').css('display', 'none');
+  $('.slide9').fadeIn(1000);
+});
+
+$(".next11").on("click", function(){
+  $(".slide11").hide()
+  $('.slide12').css('display', 'none');
+  $('.slide12').fadeIn(1000);
+});
+
+$(".prev11").on("click", function(){
+  $(".slide11").hide()
+  $('.slide10').css('display', 'none');
+  $('.slide10').fadeIn(1000);
+});
+
+$(".next12").on("click", function(){
+  $(".slide12").hide()
+  $('.slide13').css('display', 'none');
+  $('.slide13').fadeIn(1000);
+});
+
+$(".prev12").on("click", function(){
+  $(".slide12").hide()
+  $('.slide11').css('display', 'none');
+  $('.slide11').fadeIn(1000);
+});
+
+$(".next13").on("click", function(){
+  $(".slide13").hide()
+  $('.slide14').css('display', 'none');
+  $('.slide14').fadeIn(1000);
+});
+
+$(".prev13").on("click", function(){
+  $(".slide13").hide()
+  $('.slide12').css('display', 'none');
+  $('.slide12').fadeIn(1000);
 });
